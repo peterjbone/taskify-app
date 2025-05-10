dd590a4229d0fae7b0100aa76818c09183005c5f
peterjbone <peterjbone41@gmail.com>
2025-05-09 22:48:05 -0500

    server.js updated

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+	console.log(`server started at port ${PORT}`);
+});
