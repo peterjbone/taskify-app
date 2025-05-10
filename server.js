48b8df35368fb694d51dd31a1d4f1480d4d1b1ea
peterjbone <peterjbone41@gmail.com>
2025-05-09 22:47:17 -0500

    server.js updated

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
-app.listen;
+app.listen(PORT);
