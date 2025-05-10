28ed1348ff698918b9ca5195f6b79c96b451519f
peterjbone <peterjbone41@gmail.com>
2025-05-10 10:55:57 -0500

    server.js updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.DB_URL).then((result) => {
-	console.log();
+	console.log("DB connected successfully");
 });
 
 app.listen(PORT, () => {
