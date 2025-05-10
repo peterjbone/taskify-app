38181391bccf691ea02e0a48f07725219efa3b6b
peterjbone <peterjbone41@gmail.com>
2025-05-10 10:55:02 -0500

    server.js updated

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect();
+mongoose.connect(process.env.DB_URL);
 
 app.listen(PORT, () => {
 	console.log(`server started at port ${PORT}`);
