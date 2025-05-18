911957bbd17918d11c8853c86410ad2c1e37389e
peterjbone <peterjbone41@gmail.com>
2025-05-18 07:16:21 -0500

    User.js updated

--- models/User.js
+++ models/User.js
@@ -19,7 +19,7 @@ userSchema.pre("save", async function (next) {
 });
 
 userSchema.methods.comparePassword = async function (password) {
-	return bcrypt.compare();
+	return bcrypt.compare(password, this.password);
 };
 
 const User = models.User || mongoose.model("User", userSchema);
