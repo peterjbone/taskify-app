6563a9cb81b0b4933424770aca711cdc27b6d183
peterjbone <peterjbone41@gmail.com>
2025-05-18 07:10:13 -0500

    User.js updated

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ userSchema.pre("save", async function (next) {
 	const user = this;
 	if (!user.isModified) return next();
 	let salt = await bcrypt.genSalt(10);
-	let hash = await bcrypt.hash();
+	let hash = await bcrypt.hash(use.password, salt);
 });
 
 const User = models.User || mongoose.model("User", userSchema);
